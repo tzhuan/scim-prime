65263b2b281e56f16d89078ca0223663fceb88fd
(no author) <(no author)@567d3d68-1e22-0410-bf6b-86b333ce3943>
2005-05-17 08:11:39 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_0_2_5'.

--- src/prime_session.cpp
+++ src/prime_session.cpp
@@ -21,9 +21,12 @@
 #include "prime_session.h"
 #include "prime_commands.h"
 
-PrimeSession::PrimeSession (PrimeConnection *connection, String id_str)
+PrimeSession::PrimeSession (PrimeConnection *connection,
+                            String id_str,
+                            const char *language)
     : m_connection (connection),
-      m_id_str (id_str)
+      m_id_str (id_str),
+      m_language (language ? language : "")
 {
 }
 
@@ -80,6 +83,7 @@ PrimeSession::edit_erase (void)
 }
 
 void
+
 PrimeSession::edit_get_preedition (WideString &left,
                                    WideString &cursor,
                                    WideString &right)
@@ -90,16 +94,11 @@ PrimeSession::edit_get_preedition (WideString &left,
     String preedition;
 
     if (success) {
-        m_connection->get_reply (list, "\t");
+        m_connection->get_reply (list, "\t", 3);
 
-        if (list.size () >= 1)
-            m_connection->m_iconv.convert (left, list[0]);
-        if (list.size () >= 2)
-            m_connection->m_iconv.convert (cursor, list[1]);
-        if (list.size () >= 3)
-            m_connection->m_iconv.convert (right, list[2]);
-
-        m_preedition = left + cursor + right;
+        m_connection->m_iconv.convert (left,   list[0]);
+        m_connection->m_iconv.convert (cursor, list[1]);
+        m_connection->m_iconv.convert (right,  list[2]);
     } else {
         // error
     }
@@ -117,40 +116,265 @@ PrimeSession::edit_get_query_string (String &string)
     }
 }
 
-WideString &
+void
 PrimeSession::edit_insert (const char *str)
 {
-    bool success = send_command (PRIME_EDIT_INSERT, str);
-
-    std::vector<String> list;
-    String preedition;
-
-    if (success) {
-        m_connection->get_reply (list, "\t");
-    } else {
-        // error
-    }
-
-    for (unsigned int i = 0; i < list.size (); i++)
-        preedition += list[i];
-
-    m_connection->m_iconv.convert (m_preedition, preedition);
-
-    return m_preedition;
+    send_command (PRIME_EDIT_INSERT, str);
 }
 
 void
-PrimeSession::set_context (WideString &context)
+PrimeSession::edit_set_mode (PrimePreeditionMode mode)
+{
+    char *command = "default";
+
+    switch (mode) {
+    case PRIME_PREEDITION_KATAKANA:
+        command = "katakana";
+        break;
+    case PRIME_PREEDITION_HALF_KATAKANA:
+        command = "half_katakana";
+        break;
+    case PRIME_PREEDITION_WIDE_ASCII:
+        command = "wide_ascii";
+        break;
+    case PRIME_PREEDITION_RAW:
+        command = "raw";
+        break;
+    case PRIME_PREEDITION_DEFAULT:
+    default:
+        command = "default";
+        break;
+    }
+
+    send_command (PRIME_EDIT_SET_MODE, command);
+}
+
+void
+PrimeSession::edit_undo (void)
+{
+    send_command (PRIME_EDIT_UNDO);
+}
+
+void
+PrimeSession::conv_predict (PrimeCandidates &candidates, String method)
+{
+    bool success = send_command (PRIME_CONV_PREDICT);
+
+    if (success) {
+        get_candidates (candidates);
+    } else {
+        // error
+    }
+}
+
+void
+PrimeSession::conv_convert (PrimeCandidates &candidates, String method)
+{
+    bool success = send_command (PRIME_CONV_CONVERT);
+
+    if (success) {
+        get_candidates (candidates);
+    } else {
+        // error
+    }
+}
+
+void
+PrimeSession::get_candidates (PrimeCandidates &candidates)
+{
+    std::vector<String> cand_list;
+    m_connection->get_reply (cand_list, "\n");
+
+    std::vector<String>::iterator it = cand_list.begin ();
+
+    for (it++ /* Skip the index field. FIXME! */;
+         it != cand_list.end ();
+         it++)
+    {
+        std::vector<String> cols;
+
+        scim_prime_util_split_string (*it, cols, "\t");
+
+        candidates.push_back (PrimeCandidate ());
+        PrimeCandidate &cand = *(candidates.end () - 1);
+
+        if (cols.size () > 0)
+            m_connection->m_iconv.convert (cand.m_conversion, cols[0]);
+
+        for (unsigned int i = 1; i < cols.size (); i++) {
+            std::vector<String> pair;
+            scim_prime_util_split_string (cols[i], pair, "=", 2);
+            m_connection->m_iconv.convert (cand.m_values[pair[0]], pair[1]);
+        }
+    }
+}
+
+void
+PrimeSession::conv_select (WideString &selected_string, int index)
+{
+    char buf[32];
+    sprintf(buf, "%10d", index);
+
+    bool success = send_command (PRIME_CONV_SELECT, buf);
+
+    if (success) {
+        m_connection->get_reply (selected_string);
+    } else {
+        // error
+    }
+}
+
+void
+PrimeSession::conv_commit (WideString &commited_string)
+{
+    bool success = send_command (PRIME_CONV_COMMIT);
+
+    if (success) {
+        m_connection->get_reply (commited_string);
+    } else {
+        // error
+    }
+}
+
+void
+PrimeSession::modify_start (void)
+{
+    send_command (PRIME_MODIFY_START);
+}
+
+void
+PrimeSession::modify_cursor_left (void)
+{
+    send_command (PRIME_MODIFY_CURSOR_LEFT);
+}
+
+void
+PrimeSession::modify_cursor_left_edge (void)
+{
+    send_command (PRIME_MODIFY_CURSOR_LEFT_EDGE);
+}
+
+void
+PrimeSession::modify_cursor_right (void)
+{
+    send_command (PRIME_MODIFY_CURSOR_RIGHT);
+}
+
+void
+PrimeSession::modify_cursor_right_edge (void)
+{
+    send_command (PRIME_MODIFY_CURSOR_RIGHT_EDGE);
+}
+
+void
+PrimeSession::modify_cursor_expand (void)
+{
+    send_command (PRIME_MODIFY_CURSOR_EXPAND);
+}
+
+void
+PrimeSession::modify_cursor_shrink (void)
+{
+    send_command (PRIME_MODIFY_CURSOR_SHRINK);
+}
+
+void
+PrimeSession::modify_get_candidates (PrimeCandidates &candidates,
+                                     int &index)
+{
+    bool success = send_command (PRIME_MODIFY_GET_CANDIDATES);
+
+    if (success) {
+        get_candidates (candidates);
+    } else {
+        // error
+    }
+}
+
+void
+PrimeSession::modify_get_conversion (WideString &left,
+                                     WideString &cursor,
+                                     WideString &right)
+{
+    bool success = send_command (PRIME_MODIFY_GET_CONVERSION);
+
+    if (success) {
+        std::vector<String> cols;
+        m_connection->get_reply (cols, "\t", 3);
+
+        m_connection->m_iconv.convert (left,   cols[0]);
+        m_connection->m_iconv.convert (cursor, cols[1]);
+        m_connection->m_iconv.convert (right,  cols[2]);
+    } else {
+        // error
+    }
+}
+
+void
+PrimeSession::segment_reconvert (PrimeCandidates &candidates)
+{
+    bool success = send_command (PRIME_SEGMENT_RECONVERT);
+
+    if (success) {
+        get_candidates (candidates);
+    } else {
+        // error
+    }
+}
+
+void
+PrimeSession::segment_select (int index)
+{
+    char buf[32];
+    sprintf(buf, "%10d", index);
+    send_command (PRIME_SEGMENT_SELECT, buf);
+}
+
+void
+PrimeSession::segment_commit (void)
+{
+    send_command (PRIME_SEGMENT_COMMIT);
+}
+
+void
+PrimeSession::context_set_previous_word(WideString &word)
 {
     String str;
-    m_connection->m_iconv.convert (str, context);
-    send_command (PRIME_SET_CONTEXT, str.c_str());
+    m_connection->m_iconv.convert(str, word);
+    send_command (PRIME_CONTEXT_SET_PREVIOUS_WORD, str.c_str());
+}
+
+void
+PrimeSession::context_reset (void)
+{
+    send_command (PRIME_CONTEXT_RESET);
+}
+
+void
+PrimeSession::get_env (const String &key,
+                       String &type, std::vector<String> &values)
+{
+    bool success = send_command (PRIME_SESSION_GET_ENV, key.c_str ());
+
+    if (success) {
+        m_connection->get_reply (values, "\t");
+        if (values.size () > 0) {
+            type = values[0];
+            values.erase (values.begin());
+        }
+    } else {
+        type = "nil";
+    }
 }
 
 bool
 PrimeSession::has_preedition (void)
 {
-    if (m_preedition.length () > 0)
+    WideString left, cursor, right;
+
+    edit_get_preedition (left, cursor, right);
+
+    if (left.length () + cursor.length () + right.length () > 0)
         return true;
     else
         return false;
